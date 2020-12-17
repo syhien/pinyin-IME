036dc8a66691d6fb7a15602617daa6a9c05b30f5
syhien <syhien@outlook.at>
2020-12-17 11:45:17 +0800

    调整业务逻辑，在main.cpp里输入拼音表

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <locale>
+#include <vector>
 using namespace std;
+void pinyin2data();
 void txt2data();
+
+map<string, vector<wchar_t>> pinyin;
 
 int main()
 {
-	ifstream fin;
-	fin.open("IME.data", ios::in);
-	if (!fin)
-		txt2data();
+	pinyin2data();
 	return 0;
+}
+
+void pinyin2data()
+{
+	wifstream fin;
+	fin.imbue(locale("chs"));
+	fin.open("pinyin.txt", ios::in);
+	wstring line;
+	while (fin >> line)
+	{
+		string en = "";
+		int i;
+		for (i = 0; i < line.length() and (line[i] >= 'a' and line[i] <= 'z'); i++)
+			en += line[i];
+		for (i++; i < line.length(); i++)
+			pinyin[en].push_back(line[i]);
+		//DEBUG
+		cout << "×ÖÄ¸´®:" << en << " ";
+		wcout.imbue(locale("chs"));
+		for (auto& j : pinyin[en])
+			wcout << j << " ";
+		cout << endl;
+	}
 }
--- txt2data.cpp
+++ txt2data.cpp
@@ -1,4 +1,8 @@
 #include <fstream>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <locale>
 using namespace std;
 
 void txt2data()
